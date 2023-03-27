c202929a1d74de06919be6d8c419622fce1b2fcd
Nico <juareznico61@gmail.com>
2023-03-27 19:33:30 -0300

    chat listo

--- app.js
+++ app.js
@@ -1,43 +1,64 @@
-import "dotenv/config.js"   //configuracion del archivo .env
-import express from "express"
-import path from "path" 
-import cookieParser from "cookie-parser" 
-import logger from "morgan" 
-import indexRouter from './routes/index.js'  
-import usersRouter from './routes/users.js'
-import { __dirname } from "./utils.js"  
-import './config/database.js'    
-import cors from 'cors'
+import "dotenv/config.js"; //configuracion del archivo .env
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+import { __dirname } from "./utils.js";
+import "./config/database.js";
+import cors from "cors";
+import axios from "axios";
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      { headers: { "Private-Key": "82e728fa-c7d7-452b-bee9-26bfecf677b2" } }
+    );
+    console.log(r);
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-export default app
+export default app;
